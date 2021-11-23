2b11a2224987d66c7f71be5eee7a8095fce72cc5
labeeblabu <labeelabu7012488568@gmail.com>
2021-11-23 10:05:34 +0530

    added

--- sharpener.js
+++ sharpener.js
@@ -1,7 +1,10 @@
-let items =document.getElementsByClassName('list-gruop-items')
-console.log(items);
-items[1].textContent='hello 2'
-items[3].style.backgroundcolor='green'
-for(let i=0;i<items.length;i++){
-    items[i].style.fontweight='bold'
+<li>item 5</li>
+
+
+let li =document.getElementsByClassName('li')
+console.log(li);
+li[1].textContent='hello 2'
+li[3].style.backgroundcolor='green'
+for(let i=0;i<li.length;i++){
+    li[i].style.fontweight='bold'
 }
